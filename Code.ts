b9a9f105b3550631812af28b68f8d07a07f8da0e
Ying Cong <yingcong@glints.com>
2019-08-14 18:33:12 +0800

    Added function to reveal the scores

--- Code.ts
+++ Code.ts
@@ -3,7 +3,7 @@ import 'google-apps-script';
 function onOpen() {
   const ui = DocumentApp.getUi();
   ui.createMenu('Calculate Scores')
-  .addItem('Outcome', 'calculateOutcomeScore')
+  .addItem('Outcome', 'main')
   .addToUi();
 }
 
@@ -29,27 +29,46 @@ function getScoreTables() {
 }
 
 function averageScores(scores: number[]): number {
-  return scores.reduce((sum, score) => sum+score, 0)/scores.length;
+  return log(scores.reduce((sum, score) => sum+score, 0))/log(scores.length);
 }
 
 function getAllScoresInTable(table: GoogleAppsScript.Document.Table): number[] {
-  return new Array(table.getNumRows()).reduce((scores, _, idx) => {
+  return [...Array(table.getNumRows())].reduce((scores, _, idx) => {
     const row = table.getRow(idx);
     if (ensureRowIsForScoring(row)) {
       return [...scores, ...getScoresForRow(row)];
+    } else {
+      return scores;
     }
   }, []);
 }
 
 function getScoresForRow(tableRow: GoogleAppsScript.Document.TableRow): number[] {
-  return new Array(tableRow.getNumCells())
+  return [...Array(tableRow.getNumCells())]
     .map((_, idx) => tableRow.getCell(idx))
     .filter(tableCell => isCellForScoring(tableCell))
     .map(tableCell => parseInt(tableCell.getText()));
 }
 
+function revealAllScores(table: GoogleAppsScript.Document.Table) {
+  return [...Array(table.getNumRows())]
+    .map((_, idx) => table.getRow(idx))
+    .filter(row => ensureRowIsForScoring(row))
+    .forEach(scoringRow => {
+        [...Array(scoringRow.getNumCells())]
+        .map((_, idx) => scoringRow.getCell(idx))
+        .filter(cell => isCellForScoring)
+        .forEach(cell => cell.setBackgroundColor(null));
+    });
+}
+
+function log(value: any) {
+  Logger.log(value);
+  return value;
+}
+
 function ensureRowIsForScoring(tableRow: GoogleAppsScript.Document.TableRow): boolean {
-  return new Array(tableRow.getNumCells()).every((_, idx) => {
+  return [...Array(tableRow.getNumCells())].every((_, idx) => {
     const tableCell = tableRow.getCell(idx);
     if (idx % 2 === 1) {
       return isCellForScoring(tableCell);
@@ -62,4 +81,16 @@ function ensureRowIsForScoring(tableRow: GoogleAppsScript.Document.TableRow): bo
 function isCellForScoring(tableCell: GoogleAppsScript.Document.TableCell) {
   const cellContent = tableCell.getText();
   return Number.isInteger(parseInt(cellContent)) && parseInt(cellContent) <= 4;
+}
+
+function calculateAndUpdateTableScore(table: GoogleAppsScript.Document.Table) {
+  const averageOutcomeScore = averageScores(getAllScoresInTable(table));
+  revealAllScores(table);
+  return table.getCell(table.getNumRows() - 1, 1).setText(`${averageOutcomeScore}/4`);
+}
+
+// Entry point for calculations
+function main() {
+  const tables = getScoreTables();
+  return calculateAndUpdateTableScore(tables[0]);
 }
