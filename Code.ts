9ba04396dc4496a37bb2b55f87d8b50fbb6e031c
Ying Cong <yingcong@glints.com>
2019-08-14 17:12:54 +0800

    Added functions to checkRows are for scoring, and average the scores

--- Code.ts
+++ Code.ts
@@ -1,11 +1,65 @@
+import 'google-apps-script';
+
 function onOpen() {
   const ui = DocumentApp.getUi();
-  ui.createMenu('Calculate Score')
+  ui.createMenu('Calculate Scores')
   .addItem('Outcome', 'calculateOutcomeScore')
   .addToUi();
 }
 
-function getTables(tableTitle: string) {
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" && 
+         isFinite(value) && 
+         Math.floor(value) === value;
+};
+
+function getScoreTables() {
   const tables = DocumentApp.getActiveDocument().getBody().getTables();
-  console.log(tables);
+  return tables
+  .filter(table => table.getCell(0, 0).getText().toLowerCase().endsWith('score'));
+}
+
+function averageScores(scores: number[]): number {
+  return scores.reduce((sum, score) => sum+score, 0)/scores.length;
+}
+
+function getAllScoresInTable(table: GoogleAppsScript.Document.Table): number[] {
+  return new Array(table.getNumRows()).reduce((scores, _, idx) => {
+    const row = table.getRow(idx);
+    if (ensureRowIsForScoring(row)) {
+      return [...scores, ...getScoresForRow(row)];
+    }
+  }, []);
+}
+
+function getScoresForRow(tableRow: GoogleAppsScript.Document.TableRow): number[] {
+  return new Array(tableRow.getNumCells())
+    .map((_, idx) => tableRow.getCell(idx))
+    .filter(tableCell => isCellForScoring(tableCell))
+    .map(tableCell => parseInt(tableCell.getText()));
+}
+
+function ensureRowIsForScoring(tableRow: GoogleAppsScript.Document.TableRow): boolean {
+  return new Array(tableRow.getNumCells()).every((_, idx) => {
+    const tableCell = tableRow.getCell(idx);
+    if (idx % 2 === 1) {
+      return isCellForScoring(tableCell);
+    } else {
+      return true;
+    }
+  });
+}
+
+function isCellForScoring(tableCell: GoogleAppsScript.Document.TableCell) {
+  const cellContent = tableCell.getText();
+  return Number.isInteger(parseInt(cellContent)) && parseInt(cellContent) <= 4;
 }
